715572dcd7364bf078498957b23958ba5e163b7b
SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
2020-12-20 13:41:35 +0300

    31231

---
+++ Assets/Все что там было/Scripts/player/CameraMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform CamTransform;
+    public Transform Pivot;
+    public Transform Character;
+    public Transform MTransform;
+
+    public CharacterStatus CharacterStatus;
+    public CameraConfig CameraConfig;
+    public float Delta;
+
+    public float MouseX;
+    public float MouseY;
+    public float SmoothX;
+    public float SmoothY;
+    public float SmoothXVelocity;
+    public float SmoothYVeloctity;
+    static public float LookAngle;
+    public float TitlAngle;
+
+    private void FixedUpdate()
+    {
+        FixedTick();
+    }
+
+    private void FixedTick()
+    {
+        Delta = Time.deltaTime;
+
+        HandlePosition();
+        HandleRotation();
+
+        Vector3 targetPosition = Vector3.Lerp(MTransform.position, Character.position, 1);
+        MTransform.position = targetPosition;
+    }
+
+    public void HandlePosition()
+    {
+        float targetX = CameraConfig.NormalX;
+        float targetY = CameraConfig.NormalY;
+        float targetZ = CameraConfig.NormalZ;
+
+        if(CharacterStatus.IsAiming)
+        {
+            targetX = CameraConfig.AimX;
+            targetZ = CameraConfig.AimZ;
+        }
+
+        Vector3 newPivotPosition = Pivot.localPosition;
+        newPivotPosition.x = targetX;
+        newPivotPosition.y = targetY;
+
+        Vector3 newCameraPosition = CamTransform.localPosition;
+        newCameraPosition.z = targetZ;
+
+        float t = Delta * CameraConfig.PivotSpeed;
+        Pivot.localPosition = Vector3.Lerp(Pivot.localPosition, newPivotPosition, t);
+        CamTransform.localPosition = Vector3.Lerp(CamTransform.localPosition, newCameraPosition, t);
+    }
+
+    public void HandleRotation()
+    {
+        MouseX = Input.GetAxis("Mouse X");
+        MouseY = Input.GetAxis("Mouse Y");
+
+        if(CameraConfig.TurnSmooth > 0)
+        {
+            SmoothX = Mathf.SmoothDamp(SmoothX, MouseX, ref SmoothXVelocity, CameraConfig.TurnSmooth);
+            SmoothY = Mathf.SmoothDamp(SmoothY, MouseY, ref SmoothXVelocity, CameraConfig.TurnSmooth);
+        }
+        else 
+        {
+            SmoothX = MouseX;
+            SmoothY = MouseY;
+        }
+
+        LookAngle += SmoothX * CameraConfig.YRotSpeed;
+        Quaternion targetRotation = Quaternion.Euler(0, LookAngle, 0);
+        MTransform.rotation = targetRotation;
+
+        TitlAngle -= SmoothY * CameraConfig.YRotSpeed;
+        TitlAngle = Mathf.Clamp(TitlAngle, CameraConfig.MinAngle, CameraConfig.MaxAngle);
+        Pivot.localRotation = Quaternion.Euler(TitlAngle, 0, 0);
+     }
+}
---
+++ Assets/Все что там было/Scripts/player/Movement.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Animator anim;
+
+    private GameObject _player;
+    private Rigidbody _rb;
+
+    private float _speed;
+    private float _buster;
+    private float _maxSpeed;
+
+    private float _jumpForce;
+
+    private bool _isGrounded;
+
+    private void Start()
+    {
+    	_player = (GameObject)this.gameObject; 
+    	_rb = _player.GetComponent<Rigidbody>();
+
+        _maxSpeed = 400f;
+        _jumpForce = 200f;
+    }
+
+    private void FixedUpdate()
+    {
+    	PlayerMove();
+    	PlayerJump();
+    }
+
+    private void PlayerMove()
+    {
+    	var moveX = Input.GetAxis("Horizontal");
+    	var moveY = Input.GetAxis("Vertical");
+
+        _rb.AddForce(transform.forward * moveY * _speed * Time.deltaTime);
+        _rb.AddForce(transform.right * moveX * _speed * Time.deltaTime);
+
+        if (moveX != 0 || moveY != 0)
+        {
+            _rb.drag = 0;
+
+            if (_buster != 0 & _speed == _maxSpeed)
+            {
+                if ((_maxSpeed / 2f) >= _speed && _speed >= (_maxSpeed / 1.5f))
+                {
+                    _buster /= 1.2f;
+                }
+
+                if ((_maxSpeed / 1.49f) >= _speed && _speed >= (_maxSpeed / 1f))
+                {
+                    _buster /= 1.5f;
+                }
+            }
+
+            if (_speed < _maxSpeed)
+            {
+                _speed += _buster;
+            }
+        }
+        else
+        {
+            _buster = 20f;
+
+            if (_speed > 0)
+            {
+                _speed -= _buster;
+            }
+
+            _rb.drag = 4;
+        }
+        var LookY = Input.GetAxis("Mouse X") * 15;
+
+        if (moveX != 0 || moveY != 0)
+            _player.transform.rotation = Quaternion.Euler(0, CameraMovement.LookAngle, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsGroundedUpate(collision, true);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        IsGroundedUpate(collision, false);
+    }
+
+    private void IsGroundedUpate(Collision collision, bool value)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+            _isGrounded = value;
+    }
+
+    private void PlayerJump()
+    {
+    	if (Input.GetAxis("Jump") > 0)
+    	{
+    		if (_isGrounded)
+    			_rb.AddForce(Vector3.up * _jumpForce);
+    	}
+    }
+}
